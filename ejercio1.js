85471f03f11a36fccc670d0cdd4e10bbe7196f70
Alberto Jimenez <albertoxbox16@gmail.com>
2025-06-16 09:55:48 -0500

    Ejercicio 1 hecho

---
+++ ejercio1.js
@@ -0,0 +1,10 @@
+let name = prompt("Ingrese su nombre")
+let age = prompt("Ingrese su edad")
+
+if (age>=18){
+    alert(`Estimado ${name}, usted es mayor de edad.`)
+} else if (age<18){
+    alert(`Estimado ${name}, usted es menor de edad.`)
+} else{
+    alert(`Estimado ${name}, que carajinhos pusó?`)
+}
