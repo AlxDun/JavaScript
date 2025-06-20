3d56c23f56f7c8e6697a1b8c36d861ed636a6886
Alberto Jimenez <albertoxbox16@gmail.com>
2025-06-20 10:05:33 -0500

    Update sistema_interactivo.js

--- Semana 1/Entrenamiento/sistema_interactivo.js
+++ Semana 1/Entrenamiento/sistema_interactivo.js
@@ -6,18 +6,16 @@ let edad
 console.log("Bienvenido!")
 
 // Capturar datos del usuario.
-while(true){
-    nombre=prompt("Ingrese su nombre.")
+do {
+    nombre=prompt("Ingrese su nombre.").trim()
     if (nombre==""){console.error("Error: El campo está vacio."); alert("El campo no puede estar vacio.")}
-    else {break}
-}
+}   while(nombre=="")
 
-while(true){
+do {
     edad=parseInt(prompt("Ingrese su edad"))
     if (isNaN(edad)||edad<1){console.error("Error: por favor ingrese un numero valido."); alert("Debe ingresar un numero valido.")}
-    else{break}
-}
+}   while(isNaN(edad)||edad<1)
 
 // Validacion y mensaje
 if (edad>=18){alert(`Saludos ${nombre}, eres mayor de edad.`)}
-else if (edad<18){alert(`Saludos ${edad}, eres menor edad.`)}
+else if (edad<18){alert(`Saludos ${nombre}, eres menor edad.`)}
