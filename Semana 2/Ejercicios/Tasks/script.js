da9927cd607ccd4be3480cd21db42f990e9eddef
AlxDun <albertoxbox16@gmail.com>
2025-07-03 08:58:10 -0500

    actualizacion de la distribucion de las ramas

--- Semana 2/Ejercicios/Tasks/script.js
+++
@@ -1,8 +0,0 @@
-const getstatus = (status) => {
-    switch (status){
-        case "toDo": return "Pendiente";
-        case "inProgress": return "en progreso";
-        case "done": return "Completada";
-        default: return "Desconocido"
-    }
-}
